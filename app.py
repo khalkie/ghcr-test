df74b996850d72ceee901bdde7e1d9b52c2f6af0
Kevin Halk <khalk@google.com>
2026-01-22 11:48:22 -0500

    Add comment for future change reference
    Added a comment indicating another change for future reference.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import socketserver
 
 PORT = 8000  # You can change the port number here
 
+# this is another change for another CL
+
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
