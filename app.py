e0ea537ab11f7e52554527451f22cd74e8c55f5f
Kevin Halk <khalk@google.com>
2026-05-28 14:37:37 -0400

    Update print statement for serving port message

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ PORT = 8000  # You can change the port number here
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"serving here are changes at port {PORT}")
+    print(f"serving here are changes more changes at port {PORT}")
     httpd.serve_forever()
